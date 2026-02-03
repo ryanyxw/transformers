240a2901d02e865442a33e5c3ded362d34c72b7a
ryanyxw <ryanywan@usc.edu>
2026-02-02 16:41:46 -0800

    update small bug on num_items_in_batch

--- src/transformers/models/flex_olmo_noqknorm_prenorm/modeling_flex_olmo_noqknorm_prenorm.py
+++ src/transformers/models/flex_olmo_noqknorm_prenorm/modeling_flex_olmo_noqknorm_prenorm.py
@@ -766,8 +766,8 @@ class FlexOlmoNoQKNormPrenormForCausalLM(FlexOlmoNoQKNormPrenormPreTrainedModel,
         return MoeCausalLMOutputWithPast(
             loss=loss,
             aux_loss=lb_loss,
-            lb_loss=lb_loss.detach().clone(),  # for logging callback
-            ce_loss=ce_loss.detach().clone(),  # for logging callback
+            lb_loss=lb_loss.detach().clone() if lb_loss is not None else None,  # for logging callback
+            ce_loss=ce_loss.detach().clone() if lb_loss is not None else None,  # for logging callback
             logits=logits,
             past_key_values=outputs.past_key_values,
             hidden_states=outputs.hidden_states,
